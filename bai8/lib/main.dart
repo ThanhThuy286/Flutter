dd29a75f48a65b882e177f4b0a54f4c4cbecfae0
ThanhThuy286 <149869672+ThanhThuy286@users.noreply.github.com>
2025-03-21 13:09:41 +0700

    done

---
+++ bai8/lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() => runApp(
+      MaterialApp(
+        home: BallPage(),
+      ),
+    );
+
+class BallPage extends StatelessWidget {
+
+  final colors = [Colors.blue, Colors.red, Colors.green, Colors.orange, Colors.pink, Colors.teal, Colors.cyan];
+
+  BallPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    final color = colors[Random().nextInt(7)];
+
+    return Scaffold(
+      backgroundColor: color,
+      appBar: AppBar(
+        title: Text('Ask Me Anything'),
+        backgroundColor: color[900],
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  const Ball({super.key});
+
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+
+  int ballNo = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: TextButton(
+          child: Image.asset('images/ball$ballNo.png'),
+          onPressed: (){
+            setState(() {
+              int temp = Random().nextInt(5)+1;
+              while(temp == ballNo) {
+                temp = Random().nextInt(5)+1;
+              }
+              ballNo = temp;
+            });
+          },
+        ),
+      ),
+    );
+  }
+}
